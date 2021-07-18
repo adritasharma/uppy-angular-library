22a497cb2572b2e67c6e37854f651e5d8f4ab43f
Adrita Sharma <adritasharma.developer@gmail.com>
2021-07-18 12:18:13 +0530

    demo component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { UppyConfig } from 'projects/uppy-angular/src/lib/uppy-config';
-import { environment } from 'src/environments/environment';
-// import { UppyConfig } from 'uppy-angular';
+
 
 
 @Component({
@@ -10,52 +8,5 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Uppy Angular Demo';
-
-  settings: UppyConfig = {
-    uploadAPI: {
-      endpoint: "https://localhost:44315/api/Upload",
-      headers: {
-        Authorization: 'Bearer ' + localStorage.getItem('userToken')
-      }
-    },
-    plugins: {
-      Webcam: true,
-      GoogleDrive: true,
-      Instagram: true,
-      Facebook: true,
-      Dropbox: true,
-      ScreenCapture: true
-    },
-    restrictions: {
-      maxNumberOfFiles: 10,
-    },
-    options: {
-      debug: true,
-    },
-    uploaderLook: {
-      note: "",
-      theme: 'auto',
-      proudlyDisplayPoweredByUppy: true
-    },
-    statusBarOptions: {
-      showProgressDetails: true,
-      hideRetryButton: false,
-      hideCancelButton: false,
-      hideProgressAfterFinish: false
-    }
-  }
-
-  onFileAdd(evt) {
-    console.log("onFileAdd", evt)
-  }
-
-  onFileUpload(evt) {
-    console.log("onFileUpload", evt)
-  }
-
-  uploadResult(evt) {
-    console.log("uploadResult", evt)
-  }
-
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UppyAngularModule } from 'projects/uppy-angular/src/lib/uppy-angular.module';
 //import { UppyAngularModule } from 'uppy-angular';
+import { TestComponent } from './test/test.component';
+import { HumanCasePipe } from './pipes/human-case.pipe';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    TestComponent,
+    HumanCasePipe,
+      DemoComponent
+   ],
   imports: [
     BrowserModule,
-    UppyAngularModule
+    UppyAngularModule,
+    FormsModule,
+    NgSelectModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
